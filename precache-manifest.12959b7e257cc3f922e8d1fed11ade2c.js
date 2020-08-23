8562eb5c92125cbd5beee076ef874bb4c39e45f0
cyee20-1724226 <cyee20@uw.edu>
2020-08-22 18:33:29 -0700

    Updates

---
+++ precache-manifest.12959b7e257cc3f922e8d1fed11ade2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfd34960282db15b8ceaaa4e0b2a8a2",
+    "url": "/project-01-cyee20-1724226/project-react/index.html"
+  },
+  {
+    "revision": "1cd220603571af4efc5e",
+    "url": "/project-01-cyee20-1724226/project-react/static/css/main.d19ac242.chunk.css"
+  },
+  {
+    "revision": "2442dd0d2c8f0d455b99",
+    "url": "/project-01-cyee20-1724226/project-react/static/js/2.5e6492a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-01-cyee20-1724226/project-react/static/js/2.5e6492a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd220603571af4efc5e",
+    "url": "/project-01-cyee20-1724226/project-react/static/js/main.b6790646.chunk.js"
+  },
+  {
+    "revision": "5016abecd59f04880ca2",
+    "url": "/project-01-cyee20-1724226/project-react/static/js/runtime-main.815b0c09.js"
+  }
+]);
